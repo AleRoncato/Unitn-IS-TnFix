b2b6b077a17e43d96ed26ea85bc9c89fc28acd2a
Ciomp21 <gianluca.campi@studenti.unitn.it>
2024-11-19 14:50:57 +0100

    Fixed most of problems with the sidebar and added a card component

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,74 +1,107 @@
-import React, { useState } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
-import { CrossIcon, Moon, SidebarClose, Sun } from 'lucide-react';
-import { IconUserCircle, IconShoppingCart, IconLogin, IconLogout, IconShield, IconBurger, IconLayoutSidebar } from '@tabler/icons-react';
-import Sidebar from './sidebar';
-
-
-
+import React, { useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { CrossIcon, Moon, SidebarClose, Sun } from "lucide-react";
+import {
+  IconUserCircle,
+  IconShoppingCart,
+  IconLogin,
+  IconLogout,
+  IconShield,
+  IconBurger,
+  IconLayoutSidebar,
+} from "@tabler/icons-react";
+import Sidebar from "./sidebar";
 
 export const Header = ({ theme, toggleTheme }) => {
-    const navigate = useNavigate();
-    const [hovered, setHovered] = useState(false);
+  const navigate = useNavigate();
+  const [hovered, setHovered] = useState(false);
 
-    const [showSidebar, setShowSidebar] = useState(false);
+  const [showSidebar, setShowSidebar] = useState(false);
 
-    const toggleSidebar = () => { setShowSidebar(prev => !prev) };
+  const toggleSidebar = () => {
+    setShowSidebar((prev) => !prev);
+  };
 
+  return (
+    <div>
+      <header
+        id="outer-container page-wrap"
+        className={`${
+          theme === "dark"
+            ? "bg-neutral-900 text-white"
+            : "bg-white text-neutral-900"
+        } py-4 px-6 shadow-md top-0 left-0 w-full z-50`}
+      >
+        <nav className="flex justify-between items-center">
+          <div className="flex items-center">
+            <button
+              onClick={toggleSidebar}
+              className={`${
+                theme === "dark"
+                  ? "hover:bg-neutral-700"
+                  : "hover:bg-neutral-300"
+              } p-2 rounded-full`}
+            >
+              <IconLayoutSidebar
+                className={`${theme === "dark" ? "text-white" : "text-black"}`}
+                size={30}
+              />
+            </button>
+          </div>
 
-    return (
-        <div>
-            <header id="outer-container page-wrap" className={`${theme === 'dark' ? 'bg-neutral-900 text-white' : 'bg-white text-neutral-900'} py-4 px-6 shadow-md fixed top-0 left-0 w-full z-50`}>
+          <Sidebar
+            showSidebar={showSidebar}
+            toggleSidebar={toggleSidebar}
+            theme={theme}
+          />
 
-                <nav className="flex justify-between items-center">
+          <div
+            onClick={() => navigate("/")}
+            className="space-x-6 text-2xl font-bold text-gold cursor-pointer"
+          >
+            TN FIX
+          </div>
 
-                    <div className="flex items-center">
-                        <button onClick={toggleSidebar} className={`${theme === 'dark' ? 'hover:bg-neutral-700' : 'hover:bg-neutral-300'} p-2 rounded-full`}>
-                            <IconLayoutSidebar className={`${theme === 'dark' ? 'text-white' : 'text-black'}`} size={30} />
-                        </button>
-                    </div>
-
-                    <Sidebar showSidebar={showSidebar} toggleSidebar={toggleSidebar} theme={theme} />
-
-                    <div onClick={() => navigate('/')} className="space-x-6 text-2xl font-bold text-gold cursor-pointer">
-                        TN FIX
-                    </div>
-
-                    <ul className="flex space-x-6 items-center">
-
-                        <li style={{ marginLeft: '10px' }}>
-                            <div
-                                onMouseEnter={() => setHovered(true)}
-                                onMouseLeave={() => setHovered(false)}
-                                onClick={() => {
-
-                                }} className="mr-2 cursor-pointer flex flex-col items-center relative group">
-
-                                <>
-                                    <IconLogout
-                                        size={30}
-                                        stroke={1.2}
-                                        color={`${hovered ? 'red' : theme === 'dark' ? 'white' : 'black'}`}
-                                        className="hover:text-red-500"
-                                    />
-                                    <div className="absolute bottom-0 translate-y-full bg-gray-800 text-white text-xs rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity">
-                                        Logout
-                                    </div>
-                                </>
-
-                            </div>
-                        </li>
-                        <li>
-                            <button onClick={toggleTheme} className="p-2 rounded-full hover:bg-neutral-200 dark:hover:bg-neutral-700">
-                                {theme === 'dark' ? <Sun className="text-yellow-500" size={24} /> : <Moon className="text-neutral-500" size={24} />}
-                            </button>
-                        </li>
-                    </ul>
-
-                </nav >
-            </header >
-
-            <Outlet /> {/* Importante perche senza outlet non carica i nodi figlio del padre (sicconme questa page è children di Header [come tutta la app]) */}
-        </div >
-    );
+          <ul className="flex space-x-6 items-center">
+            <li style={{ marginLeft: "10px" }}>
+              <div
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)}
+                onClick={() => {}}
+                className="mr-2 cursor-pointer flex flex-col items-center relative group"
+              >
+                <>
+                  <IconLogout
+                    size={30}
+                    stroke={1.2}
+                    color={`${
+                      hovered ? "red" : theme === "dark" ? "white" : "black"
+                    }`}
+                    className="hover:text-red-500"
+                  />
+                  <div className="absolute bottom-0 translate-y-full bg-gray-800 text-white text-xs rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity">
+                    Logout
+                  </div>
+                </>
+              </div>
+            </li>
+            <li>
+              <button
+                onClick={toggleTheme}
+                className="p-2 rounded-full hover:bg-neutral-200 dark:hover:bg-neutral-700"
+              >
+                {theme === "dark" ? (
+                  <Sun className="text-yellow-500" size={24} />
+                ) : (
+                  <Moon className="text-neutral-500" size={24} />
+                )}
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Outlet />
+      {/* Importante perche senza outlet non carica i nodi figlio del padre (sicconme questa page è children di Header [come tutta la app]) */}
+    </div>
+  );
 };
--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,72 +1,90 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { CSSTransition } from "react-transition-group";
-import { useNavigate } from 'react-router-dom';
-import { SidebarClose } from 'lucide-react';
-import { Sidenav, Nav } from 'rsuite';
+import { Outlet, useNavigate } from "react-router-dom";
+import { SidebarClose } from "lucide-react";
+import { Sidenav, Nav } from "rsuite";
 import "./header.css";
 
-
 const Sidebar = ({ showSidebar, toggleSidebar, theme }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
+  const stati = [
+    { title: "Attivi" },
+    { title: "In Svolgimento" },
+    { title: "Programmabili" },
+    { title: "Accettabili" },
+    { title: "Chiusi" },
+  ];
 
+  const tipi = [
+    { title: "Incidenti" },
+    { title: "Richieste di Servizio" },
+    { title: "Problemi" },
+    { title: "Cambiamenti" },
+    { title: "Conoscenza" },
+  ];
 
-    const stati = [
-        { title: "Attivi" },
-        { title: "In Svolgimento" },
-        { title: "Programmabili" },
-        { title: "Accettabili" },
-        { title: "Chiusi" }
-    ];
-
-    const tipi = [
-        { title: "Incidenti" },
-        { title: "Richieste di Servizio" },
-        { title: "Problemi" },
-        { title: "Cambiamenti" },
-        { title: "Conoscenza" }
-    ];
-
-    return (
-        <>
-            <CSSTransition
-                in={showSidebar}
-                timeout={1000}
-                classNames="fade"
-                unmountOnExit
+  return (
+    <>
+      <CSSTransition
+        in={showSidebar}
+        timeout={1000}
+        classNames="fade"
+        unmountOnExit
+      >
+        <div
+          onClick={toggleSidebar}
+          className={`fixed pl-80 inset-0 bg-black bg-opacity-50 z-40 ${
+            !showSidebar ? "min-w-0" : ""
+          }`}
+        >
+          <div
+            className={`${theme === "dark" ? "bg-neutral-900" : "bg-white"} 
+                        fixed left-0 top-0 h-full w-96 shadow-lg z-35 p-4 flex flex-col 
+                        overflow-y-auto scroll-m-0`}
+          >
+            <button
+              className={`${
+                theme === "dark"
+                  ? "hover:bg-neutral-700"
+                  : "hover:bg-neutral-300"
+              } ml-2 p-2 rounded-full self-start`}
             >
-                <div className={`fixed pl-80 inset-0 bg-black bg-opacity-50 z-40 ${!showSidebar ? 'min-w-0' : ''}`} >
+              <SidebarClose
+                className={`${theme === "dark" ? "text-white" : "text-black"}`}
+                size={30}
+              />
+            </button>
 
-                    <div className={`${theme === 'dark' ? 'bg-neutral-900' : 'bg-white'} 
-                        fixed left-0 top-0 h-full w-80 shadow-lg z-35 p-4 flex flex-col 
-                        overflow-y-auto scroll-m-0`}>
-
-
-                        <button onClick={toggleSidebar} className={`${theme === 'dark' ? 'hover:bg-neutral-700' : 'hover:bg-neutral-300'} ml-2 p-2 rounded-full self-start`}>
-                            <SidebarClose className={`${theme === 'dark' ? 'text-white' : 'text-black'}`} size={30} />
-                        </button>
-
-                        <div className="flex-grow flex flex-col justify-center items-center">
-                            <Sidenav>
-                                <Sidenav.Body>
-                                    <Nav>
-                                        {stati.map((item, index) => (
-                                            <Nav.Menu eventKey={index} title={item.title} >
-
-                                                {tipi.map((item, index) => (
-                                                    <Nav.Item eventKey={`3-${index}`}>{item.title}</Nav.Item>), [tipi])}
-
-                                            </Nav.Menu>), [stati])}
-
-                                    </Nav>
-                                </Sidenav.Body>
-                            </Sidenav>
-                        </div>
-                    </div>
-                </div>
-            </CSSTransition >
-        </>
-    );
+            <div className="flex-grow flex flex-col justify-center items-center">
+              {/* <Sidenav>
+                <Sidenav.Body>
+                  <Nav>
+                    {stati.map(
+                      (item, index) => (
+                        <Nav.Menu eventKey={index} title={item.title}>
+                          {tipi.map(
+                            (item, index) => (
+                              <Nav.Item eventKey={`3-${index}`}>
+                                {item.title}
+                              </Nav.Item>
+                            ),
+                            []
+                          )}
+                        </Nav.Menu>
+                      ),
+                      []
+                    )}
+                  </Nav>
+                </Sidenav.Body>
+              </Sidenav> */}
+            </div>
+          </div>
+        </div>
+      </CSSTransition>
+      <Outlet />
+    </>
+  );
 };
 
 export default Sidebar;

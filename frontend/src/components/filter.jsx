3563a1ef3f13957940a148daaed3dd963d3a04ac
Gianluca Campi <gianluca.campi@studenti.unitn.it>
2024-11-23 20:43:50 +0100

    Added the menu on the left for the filters

---
+++ frontend/src/components/filter.jsx
@@ -0,0 +1,71 @@
+import { ArrowDownIcon } from 'lucide-react';
+import React, { useEffect, useState } from 'react';
+
+const Filter = ({ theme }) => {
+    const [query, setQuery] = useState('');
+    const [fetchedItems, setFetchedItems] = useState(["2024", "2023", "2022", "2021", "2021", "2021", "2021", "2021", "2021", "2021", "2021", "2021", "2021", "2021", "2021", "2021"]);
+
+    const [isVisible, setIsVisible] = useState([]);
+
+    useEffect(() => {
+        const fetchItems = async () => {
+            try {
+                const response = await fetch('https://api.example.com/items');
+                const data = await response.json();
+                setFetchedItems(data);
+            } catch (error) {
+                console.error('Error fetching items:', error);
+            }
+        };
+
+        fetchItems();
+    }, []);
+
+    useEffect(() => {
+        setIsVisible(fetchedItems.map(() => false));
+    }, []);
+
+
+
+
+    const toggleVisibility = (index) => {
+
+        setIsVisible((prev) => prev.map((isVisible, i) => (i === index ? !isVisible : isVisible)));
+    };
+
+    return (
+        <div className="h-full">
+
+            <ul className='h-full overflow-hidden flex-col'>
+                {fetchedItems.map((item, index) => (
+                    <li key={index} onClick={() => toggleVisibility(index)}
+                        className={`text-lg font-mono font-semibold flex flex-col items-center`}
+                    >
+                        <div className={'flex items-center justify-center w-full hover:text-neutral-400'}>{item}</div>
+
+                        {isVisible[index] && (
+
+                            <ul onClick={() => toggleVisibility(index)} className={`flex flex-col items-center justify-center my-2 font-semibold text-sm`}>
+                                <li className='hover:text-neutral-400'>Active</li>
+                                <li className='hover:text-neutral-400'>Pending</li>
+                                <li className='hover:text-neutral-400'>Resolved</li>
+                                <li className='hover:text-neutral-400'>Closed</li>
+                                <li className='hover:text-neutral-400'>All</li>
+                            </ul>
+                        )}
+                    </li>
+                ))}
+            </ul>
+
+        </div>
+    );
+
+
+
+
+
+};
+
+
+
+export default Filter;
--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,92 +1,51 @@
 import React, { useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { SidebarClose } from "lucide-react";
-import "./header.css";
+import Filter from "./filter";
 
 const Sidebar = ({ showSidebar, toggleSidebar, theme }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const stati = [
-        { title: "Attivi" },
-        { title: "In Svolgimento" },
-        { title: "Programmabili" },
-        { title: "Accettabili" },
-        { title: "Chiusi" },
-    ];
-
-    const tipi = [
-        { title: "Incidenti" },
-        { title: "Richieste di Servizio" },
-        { title: "Problemi" },
-        { title: "Cambiamenti" },
-        { title: "Conoscenza" },
-    ];
-
-    return (
-        <>
+  return (
+    <>
 
 
-            {/* <CSSTransition
-        in={showSidebar}
-        timeout={1000}
-        classNames="fade"
-        unmountOnExit
-      > */}
-
-            {showSidebar &&
-                <div
-                    onClick={toggleSidebar}
-                    className={`fixed pl-80 inset-0 bg-black bg-opacity-50 z-40 ${!showSidebar ? "min-w-0" : ""
-                        }`}
-                >
-                    <div
-                        className={`${theme === "dark" ? "bg-neutral-900" : "bg-white"} 
+      {showSidebar &&
+        <div
+          onClick={toggleSidebar}
+          className={`fixed pl-80 inset-0 bg-black bg-opacity-50 z-40 ${!showSidebar ? "min-w-0" : ""
+            }`}
+        >
+          <div
+            onClick={toggleSidebar}
+            className={`${theme === "dark" ? "bg-neutral-900" : "bg-white"} 
                         fixed left-0 top-0 h-full w-96 shadow-lg z-35 p-4 flex flex-col 
                         overflow-y-auto scroll-m-0`}
-                    >
-                        <button
-                            className={`${theme === "dark"
-                                    ? "hover:bg-neutral-700"
-                                    : "hover:bg-neutral-300"
-                                } ml-2 p-2 rounded-full self-start`}
-                        >
-                            <SidebarClose
-                                className={`${theme === "dark" ? "text-white" : "text-black"}`}
-                                size={30}
-                            />
-                        </button>
+          >
+            <button
+              onClick={toggleSidebar}
+              className={`${theme === "dark"
+                ? "hover:bg-neutral-700"
+                : "hover:bg-neutral-300"
+                } ml-2 p-2 rounded-full self-start`}
+            >
+              <SidebarClose
+                className={`${theme === "dark" ? "text-white" : "text-black"}`}
+                size={30}
+              />
+            </button>
 
-                        <div className="flex-grow flex flex-col justify-center items-center">
-                            {/* <Sidenav>
-                <Sidenav.Body>
-                  <Nav>
-                    {stati.map(
-                      (item, index) => (
-                        <Nav.Menu eventKey={index} title={item.title}>
-                          {tipi.map(
-                            (item, index) => (
-                              <Nav.Item eventKey={`3-${index}`}>
-                                {item.title}
-                              </Nav.Item>
-                            ),
-                            []
-                          )}
-                        </Nav.Menu>
-                      ),
-                      []
-                    )}
-                  </Nav>
-                </Sidenav.Body>
-              </Sidenav> */}
-                        </div>
-                    </div>
-                </div>
-            }
-            {/* </CSSTransition> */}
+            <div className="h-full">
+              <Filter theme={theme} />
+            </div>
+          </div>
+        </div>
+      }
 
-            <Outlet />
-        </>
-    );
+
+      <Outlet />
+    </>
+  );
 };
 
 export default Sidebar;
